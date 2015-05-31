d46eb18366edbd0bb7712e6fac4d18cd2bc79f22
Robert Giacinto <robert.giacinto@gmail.com>
2015-05-31 11:58:39 +0200

    Add default implementations for each required module

--- app/LwmApplicationLoader.scala
+++ app/LwmApplicationLoader.scala
@@ -1,15 +1,13 @@
-import java.io.File
-
-import controllers.{Assets, HomepageController, DegreeCRUDController}
-import play.api.routing.{SimpleRouter, Router}
-import play.api.{BuiltInComponentsFromContext, Application, ApplicationLoader}
+import controllers.{Assets, DegreeCRUDController, HomepageController}
 import play.api.ApplicationLoader.Context
+import play.api.routing.Router
+import play.api.{Application, ApplicationLoader, BuiltInComponentsFromContext}
 import router.Routes
-import store.{Namespace, SesameRepository, SemanticRepository}
+import store.{Namespace, SesameRepository}
 
 class LwmApplicationLoader extends ApplicationLoader {
   def load(context: Context): Application = {
-    new LwmApplication(context).application
+    new DefaultLwmApplication(context).application
   }
 }
 
@@ -20,7 +18,7 @@ trait SemanticRepositoryModule {
   def namespace: Namespace
 }
 
-trait DefaultSemanticRepositoryModule extends SemanticRepositoryModule {
+trait DefaultSemanticRepositoryModuleImpl extends SemanticRepositoryModule {
   self: LwmApplication =>
   def namespace: Namespace = Namespace("http://lwm/")
 
@@ -29,20 +27,42 @@ trait DefaultSemanticRepositoryModule extends SemanticRepositoryModule {
 
 trait DegreeManagementModule {
   self: LwmApplication with SemanticRepositoryModule =>
-  lazy val degreeManagementController = new DegreeCRUDController(semanticRepository, namespace)
+  def degreeManagementController: DegreeCRUDController
+}
+
+
+trait DefaultDegreeManagementModuleImpl extends DegreeManagementModule {
+  self: LwmApplication with SemanticRepositoryModule =>
+  lazy val degreeManagementController: DegreeCRUDController = new DegreeCRUDController(semanticRepository, namespace)
+}
+
+trait DefaultHomepageModuleImpl extends HomepageModule {
+  self: LwmApplication =>
+  lazy val homepageController = new HomepageController
 }
 
 trait HomepageModule {
   self: LwmApplication =>
-  lazy val homepage = new HomepageController
+  def homepageController: HomepageController
 }
 
 trait AssetsModule {
   self: LwmApplication =>
+  def assetsController: Assets
+}
+
+trait DefaultAssetsModuleImpl extends AssetsModule {
+  self: LwmApplication =>
   lazy val assetsController = new Assets(httpErrorHandler)
 }
 
-class LwmApplication(context: Context) extends BuiltInComponentsFromContext(context)
-with HomepageModule with DefaultSemanticRepositoryModule with DegreeManagementModule with AssetsModule {
-  lazy val router: Router = new Routes(httpErrorHandler, homepage, degreeManagementController, assetsController)
+trait LwmApplication extends BuiltInComponentsFromContext {
+  self: HomepageModule with SemanticRepositoryModule with DegreeManagementModule with AssetsModule =>
+  lazy val router: Router = new Routes(httpErrorHandler, homepageController, degreeManagementController, assetsController)
 }
+
+class DefaultLwmApplication(context: Context) extends LwmApplication(context)
+with DefaultSemanticRepositoryModuleImpl
+with DefaultHomepageModuleImpl
+with DefaultDegreeManagementModuleImpl
+with DefaultAssetsModuleImpl
