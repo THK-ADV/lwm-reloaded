30a9ab5834c0b14c089490a10b42143bd323cb3b
Robert Marius Avram <am11.robert@yahoo.com>
2015-09-01 21:19:55 +0200

    Expanded and added API tests

--- app/utils/Ops.scala
+++ app/utils/Ops.scala
@@ -7,7 +7,7 @@ import scala.util.Try
 
 object Ops {
 
-  implicit val optApplicative = new Applicative[Option] {
+  implicit val optApplicative = new Monad[Option] {
     override def apply[A](a: A): Option[A] = Option(a)
 
     override def flatMap[A, B](z: Option[A])(f: (A) => Option[B]): Option[B] = z flatMap f
@@ -15,7 +15,7 @@ object Ops {
     override def map[A, B](z: Option[A])(f: (A) => B): Option[B] = z map f
   }
 
-  implicit val tryApplicative = new Applicative[Try] {
+  implicit val tryApplicative = new Monad[Try] {
     override def apply[A](a: A): Try[A] = Try(a)
 
     override def flatMap[A, B](z: Try[A])(f: (A) => Try[B]): Try[B] = z flatMap f
@@ -23,7 +23,7 @@ object Ops {
     override def map[A, B](z: Try[A])(f: (A) => B): Try[B] = z map f
   }
 
-  def sequence[F[+_], A](z: Vector[F[A]])(implicit AF: Applicative[F]): F[Vector[A]] = {
+  def sequence[F[+_], A](z: Vector[F[A]])(implicit AF: Monad[F]): F[Vector[A]] = {
     import AF._
     @tailrec
     def go(toGo: Vector[F[A]], soFar: F[Vector[A]]): F[Vector[A]] = {
@@ -36,7 +36,7 @@ object Ops {
   }
 }
 
-trait Applicative[F[+_]] {
+trait Monad[F[+_]] {
   def apply[A](a: A): F[A]
   def map[A, B](z: F[A])(f: A => B): F[B]
   def flatMap[A, B](z: F[A])(f: A => F[B]): F[B]
---
+++ test/store/SDSLSpec.scala
@@ -0,0 +1,83 @@
+package store
+
+import base.TestBaseDefinition
+import org.scalatest.WordSpec
+import org.w3.banana.sesame.SesameModule
+import store.sparql._
+import store.sparql.select._
+
+class SDSLSpec extends WordSpec with TestBaseDefinition with SesameModule {
+
+  def trim: String => String = s => s.replace(" ", "")
+
+  val simpleQuery = "SELECT ?a ?b ?c WHERE { ?a <isDefined> \"concretely\" . ?b <isDefined> \"not\" . ?c <isDefined> \"notReally\" .}"
+  val complexQuery = "SELECT ?a ?b ?c WHERE { ?a <isDefined> \"concretely\" . ?b <isDefined> \"not\" . FILTER(?b >= \"2\") . OPTIONAL { ?c <isDefined> \"notReally\" . } . } ORDER BY ?b"
+  val askQuery = "ASK { ?a <isDefined> \"concretely\" . ?b <isDefined> \"not\" . }"
+
+  "A SPARQL DSL" should {
+
+    "allow simple select queries" in {
+      val query =
+        (select("a", "b", "c") where {
+          ^(v("a"), p("isDefined"), o("concretely")).
+            ^(v("b"), p("isDefined"), o("not")).
+            ^(v("c"), p("isDefined"), o("notReally"))
+        }).run
+
+      trim(query).toLowerCase shouldBe trim(simpleQuery).toLowerCase
+    }
+
+
+    "allow more complex select queries with additional nested and concatenated clauses" in {
+      val query =
+        (select("a", "b", "c") where {
+          ^(v("a"), p("isDefined"), o("concretely")).
+            ^(v("b"), p("isDefined"), o("not")).
+            filter(v("b") >= o("2")).
+            optional {
+              ^(v("c"), p("isDefined"), o("notReally"))
+            }
+        } orderby "b").run
+
+      trim(query).toLowerCase shouldBe trim(complexQuery).toLowerCase
+    }
+
+    "allow ask queries" in {
+      val query =
+        ask {
+          ^(v("a"), p("isDefined"), o("concretely")).
+            ^(v("b"), p("isDefined"), o("not"))
+        }.run
+
+      trim(query).toLowerCase shouldBe trim(askQuery).toLowerCase
+    }
+
+    "generate correct select queries" in {
+      val query =
+        (select("a", "b", "c") where {
+          ^(v("a"), p("isDefined"), o("concretely")).
+            ^(v("b"), p("isDefined"), o("not")).
+            filter(v("b") >= o("2")).
+            optional {
+              ^(v("c"), p("isDefined"), o("notReally"))
+            }
+        } orderby "b").run
+
+      val res = sparqlOps.parseSelect(query)
+
+      res.isSuccess shouldBe true
+    }
+
+    "generate correct ask queries" in {
+      val query =
+        ask {
+          ^(v("a"), p("isDefined"), o("concretely")).
+            ^(v("b"), p("isDefined"), o("not"))
+        }.run
+
+      val res = sparqlOps.parseAsk(query)
+
+      res.isSuccess shouldBe true
+    }
+  }
+}
