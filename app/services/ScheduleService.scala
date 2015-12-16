31034ea850dfcaaeda5b880025e354489b04d4b5
Alexander Dobrynin <info@alex-dobrynin.com>
2015-12-16 15:36:35 +0100

    first implementation of mutate function

--- app/services/ScheduleService.scala
+++ app/services/ScheduleService.scala
@@ -7,12 +7,14 @@ trait ScheduleServiceLike {
 
   def applyBlacklist(timetable: Timetable): Timetable
 
-  def populate(amount: Int, timetable: Timetable, groups: Set[Group]): Vector[Schedule]
+  def populate(times: Int, timetable: Timetable, groups: Set[Group]): Vector[Schedule]
+
+  def mutate(schedule: Schedule): Schedule
 }
 
 class ScheduleService extends ScheduleServiceLike {
 
-  override def populate(amount: Int, timetable: Timetable, groups: Set[Group]): Vector[Schedule] = (0 until amount).map(_ => populate(timetable, groups)).toVector
+  override def populate(times: Int, timetable: Timetable, groups: Set[Group]): Vector[Schedule] = (0 until times).map(_ => populate(timetable, groups)).toVector
 
   private def populate(timetable: Timetable, groups: Set[Group]): Schedule = {
     import scala.util.Random._
@@ -25,4 +27,16 @@ class ScheduleService extends ScheduleServiceLike {
   }
 
   override def applyBlacklist(timetable: Timetable): Timetable = timetable
+
+  override def mutate(schedule: Schedule): Schedule = {
+    import scala.util.Random._
+
+    val a = schedule.entries.toVector
+
+    val swapped = schedule.entries.zip(shuffle(schedule.entries.toVector.map(_.group))).map {
+      case (entry, group) => ScheduleEntry(entry.start, entry.end, entry.day, entry.date, entry.room, entry.supervisor, group, entry.id)
+    }
+
+    Schedule(schedule.labwork, swapped, schedule.id)
+  }
 }
--- test/services/ScheduleServiceSpec.scala
+++ test/services/ScheduleServiceSpec.scala
@@ -44,7 +44,7 @@ class ScheduleServiceSpec extends WordSpec with TestBaseDefinition {
       result.foreach(s => s.entries.count(_.group == CID) shouldBe 2)
     }
 
-    "populate initial schedules any time" in {
+    "populate initial schedules any times" in {
       val entries = Set(
         TimetableEntry(Employee.randomUUID, Room.randomUUID, Degree.randomUUID, DateTime.now, DateTime.now, DateTime.now, DateTime.now, TimetableEntry.randomUUID),
         TimetableEntry(Employee.randomUUID, Room.randomUUID, Degree.randomUUID, DateTime.now, DateTime.now, DateTime.now, DateTime.now, TimetableEntry.randomUUID),
@@ -72,6 +72,39 @@ class ScheduleServiceSpec extends WordSpec with TestBaseDefinition {
       result.foreach(s => s.entries.count(_.group == AID) shouldBe 2)
       result.foreach(s => s.entries.count(_.group == BID) shouldBe 2)
       result.foreach(s => s.entries.count(_.group == CID) shouldBe 2)
+    }
+
+    "mutate given schedule by swapping groups" in {
+      val entries = Set(
+        TimetableEntry(Employee.randomUUID, Room.randomUUID, Degree.randomUUID, DateTime.now, DateTime.now, DateTime.now, DateTime.now, TimetableEntry.randomUUID),
+        TimetableEntry(Employee.randomUUID, Room.randomUUID, Degree.randomUUID, DateTime.now, DateTime.now, DateTime.now, DateTime.now, TimetableEntry.randomUUID),
+        TimetableEntry(Employee.randomUUID, Room.randomUUID, Degree.randomUUID, DateTime.now, DateTime.now, DateTime.now, DateTime.now, TimetableEntry.randomUUID),
+        TimetableEntry(Employee.randomUUID, Room.randomUUID, Degree.randomUUID, DateTime.now, DateTime.now, DateTime.now, DateTime.now, TimetableEntry.randomUUID),
+        TimetableEntry(Employee.randomUUID, Room.randomUUID, Degree.randomUUID, DateTime.now, DateTime.now, DateTime.now, DateTime.now, TimetableEntry.randomUUID),
+        TimetableEntry(Employee.randomUUID, Room.randomUUID, Degree.randomUUID, DateTime.now, DateTime.now, DateTime.now, DateTime.now, TimetableEntry.randomUUID),
+        TimetableEntry(Employee.randomUUID, Room.randomUUID, Degree.randomUUID, DateTime.now, DateTime.now, DateTime.now, DateTime.now, TimetableEntry.randomUUID),
+        TimetableEntry(Employee.randomUUID, Room.randomUUID, Degree.randomUUID, DateTime.now, DateTime.now, DateTime.now, DateTime.now, TimetableEntry.randomUUID),
+        TimetableEntry(Employee.randomUUID, Room.randomUUID, Degree.randomUUID, DateTime.now, DateTime.now, DateTime.now, DateTime.now, TimetableEntry.randomUUID),
+        TimetableEntry(Employee.randomUUID, Room.randomUUID, Degree.randomUUID, DateTime.now, DateTime.now, DateTime.now, DateTime.now, TimetableEntry.randomUUID),
+        TimetableEntry(Employee.randomUUID, Room.randomUUID, Degree.randomUUID, DateTime.now, DateTime.now, DateTime.now, DateTime.now, TimetableEntry.randomUUID),
+        TimetableEntry(Employee.randomUUID, Room.randomUUID, Degree.randomUUID, DateTime.now, DateTime.now, DateTime.now, DateTime.now, TimetableEntry.randomUUID),
+        TimetableEntry(Employee.randomUUID, Room.randomUUID, Degree.randomUUID, DateTime.now, DateTime.now, DateTime.now, DateTime.now, TimetableEntry.randomUUID),
+        TimetableEntry(Employee.randomUUID, Room.randomUUID, Degree.randomUUID, DateTime.now, DateTime.now, DateTime.now, DateTime.now, TimetableEntry.randomUUID),
+        TimetableEntry(Employee.randomUUID, Room.randomUUID, Degree.randomUUID, DateTime.now, DateTime.now, DateTime.now, DateTime.now, TimetableEntry.randomUUID)
+      )
+      val timetable = Timetable(Labwork.randomUUID, entries, DateTime.now, Set.empty[DateTime], 0, Timetable.randomUUID)
+
+      val groups = Set(
+        Group("A", Labwork.randomUUID, Set(UUID.randomUUID, UUID.randomUUID, UUID.randomUUID), Group.randomUUID),
+        Group("B", Labwork.randomUUID, Set(UUID.randomUUID, UUID.randomUUID, UUID.randomUUID), Group.randomUUID),
+        Group("C", Labwork.randomUUID, Set(UUID.randomUUID, UUID.randomUUID, UUID.randomUUID), Group.randomUUID)
+      )
+
+      val schedule = scheduleService.populate(1, timetable, groups).head
+
+      val result = (0 until 10).map(_ => scheduleService.mutate(schedule)).toVector
+
+      result.foreach(s => s.entries.toVector.map(_.group) should not be schedule.entries.toVector.map(_.group))
     }
   }
 }
