6228c2e8200488af8bb09d1448dfe02da5a3e137
Alexander Dobrynin <info@alex-dobrynin.com>
2017-10-20 11:16:04 +0200

    added missing filter to LabworkApplicationController (#52)

--- app/controllers/LabworkApplicationControllerPostgres.scala
+++ app/controllers/LabworkApplicationControllerPostgres.scala
@@ -2,7 +2,7 @@ package controllers
 
 import java.util.UUID
 
-import dao.{AuthorityDao, LabworkApplicationApplicantFilter, LabworkApplicationDao, LabworkApplicationLabworkFilter}
+import dao._
 import models.Role.{Employee, Student}
 import models._
 import play.api.libs.json.{Reads, Writes}
@@ -15,10 +15,8 @@ import scala.util.{Failure, Try}
 object LabworkApplicationControllerPostgres {
   lazy val labworkAttribute = "labwork"
   lazy val applicantAttribute = "applicant"
-  lazy val friendAttribute = "friend"
-  lazy val dateAttribute = "date"
-  lazy val minTimeAttribute = "minTime"
-  lazy val maxTimeAttribute = "maxTime"
+  lazy val sinceAttribute = "since"
+  lazy val untilAttribute = "until"
 }
 
 final class LabworkApplicationControllerPostgres(val sessionService: SessionHandlingService, val authorityDao: AuthorityDao, val abstractDao: LabworkApplicationDao)
@@ -31,9 +29,11 @@ final class LabworkApplicationControllerPostgres(val sessionService: SessionHand
   override protected def tableFilter(attribute: String, value: String)(appendTo: Try[List[TableFilter[LabworkApplicationTable]]]): Try[List[TableFilter[LabworkApplicationTable]]] = {
     import controllers.LabworkApplicationControllerPostgres._
 
-    (appendTo, (attribute, value)) match { // TODO expand attributes
+    (appendTo, (attribute, value)) match {
       case (list, (`labworkAttribute`, labwork)) => list.map(_.+:(LabworkApplicationLabworkFilter(labwork)))
       case (list, (`applicantAttribute`, applicant)) => list.map(_.+:(LabworkApplicationApplicantFilter(applicant)))
+      case (list, (`sinceAttribute`, since)) => Try(since.toLong).flatMap(l => list.map(_.+:(LabworkApplicationSinceFilter(l.toString))))
+      case (list, (`untilAttribute`, until)) => Try(until.toLong).flatMap(l => list.map(_.+:(LabworkApplicationUntilFilter(l.toString))))
       case _ => Failure(new Throwable("Unknown attribute"))
     }
   }
