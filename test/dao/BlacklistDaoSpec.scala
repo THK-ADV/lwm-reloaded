2e960d42ef65dee9a1d986a5a0e1f7dbd2316297
Alexander Dobrynin <info@alex-dobrynin.com>
2019-04-15 16:09:32 +0200

    remove unnecessary filter tests of blacklist dao

--- test/dao/BlacklistDaoSpec.scala
+++ test/dao/BlacklistDaoSpec.scala
@@ -13,54 +13,6 @@ final class BlacklistDaoSpec extends AbstractDaoSpec[BlacklistTable, BlacklistDb
 
   val dao = app.injector.instanceOf(classOf[BlacklistDao])
 
-  "A BlacklistService2Spec also" should { // TODO not sure which strings needs to be passed to those filters... plain string date or time, currentMillis, localized or not? this needs to be discussed first
-
-    /*    "filter by global blacklists" in {
-          runAsync(dao.filterBy(List(BlacklistGlobalFilter(true.toString))).result) { blacklists =>
-            blacklists shouldBe dbEntities.filter(_.global)
-          }
-        }
-
-        "filter by date" in {
-          val dateFilter = randomBlacklist.date
-          runAsync(dao.filterBy(List(BlacklistDateFilter(dateFilter.stringMillis))).result) { blacklists =>
-            blacklists shouldBe dbEntities.filter(_.date.localDate == dateFilter.localDate)
-          }
-        }
-
-        "filter by start time" in {
-          val startFilter = randomBlacklist.start.localTime
-          println(s"startFilter $startFilter")
-          runAsync(dao.filterBy(List(BlacklistStartFilter(startFilter.stringMillis))).result) { blacklists =>
-            blacklists shouldBe dbEntities.filter(_.start.localTime == startFilter)
-          }
-        }
-
-        "filter by end time" in {
-          val endFilter = randomBlacklist.end.localTime
-          println(s"endFilter $endFilter")
-          runAsync(dao.filterBy(List(BlacklistEndFilter(endFilter.stringMillis))).result) { blacklists =>
-            blacklists shouldBe dbEntities.filter(_.end.localTime == endFilter)
-          }
-        }
-
-        "filter by range" in {
-          val e = dbEntities.sortBy(_.date.localDate).slice(3, 8)
-          val since = e.head.date
-          val until = e.last.date
-
-          runAsync(dao.filterBy(List(BlacklistSinceFilter(since.stringMillis), BlacklistUntilFilter(until.stringMillis))).result) { blacklists =>
-            blacklists.forall { b =>
-              val date = b.date.localDate
-              val lower = since.localDate
-              val upper = until.localDate
-
-              (date.isAfter(lower) || date.isEqual(lower)) && (date.isBefore(upper) || date.isEqual(upper))
-            } shouldBe true
-          }
-        }*/
-  }
-
   override protected def name: String = "blacklist"
 
   override protected val dbEntity: BlacklistDb = BlacklistDb.entireDay("label", LocalDate.now.sqlDate, global = true)
