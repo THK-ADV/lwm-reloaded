79f7e6c4ab3938f5da2a871d0e0120aba6e15d01
Alexander Dobrynin <info@alex-dobrynin.com>
2019-05-08 17:42:01 +0200

    improved BackupServiceActor. added BackupServiceSpecs

--- test/service/BackupServiceSpec.scala
+++ test/service/BackupServiceSpec.scala
@@ -1,49 +1,44 @@
 package service
 
-/*import java.io.File
+import java.io.File
 
-import akka.util.Timeout
-import base.TestBaseDefinition
+import base.DatabaseSpec
 import dao._
 import org.apache.commons.io.FileUtils
 import org.mockito.Mockito._
-import org.scalatest.WordSpec
-import org.scalatest.mock.MockitoSugar.mock
-import play.api.libs.json.{JsValue, Json, Writes}
-import services.backup.{BackupItem, BackupService, PSQLBackupService}
+import org.scalatest.TryValues
+import org.scalatest.mockito.MockitoSugar
+import play.api.inject.guice.GuiceableModule
+import play.api.libs.json.{Json, Writes}
+import service.backup.{BackupItem, BackupService, PSQLBackupService}
 
-import scala.concurrent.{Await, Future}
-import scala.util.{Failure, Success, Try}
+import scala.concurrent.Future
+import scala.util.Try
 
-final class BackupServiceSpec extends WordSpec with TestBaseDefinition {
+final class BackupServiceSpec extends DatabaseSpec with MockitoSugar with TryValues {
 
   import dao.AbstractDaoSpec._
   import models.UniqueEntity.toJson
-  import scala.concurrent.duration._
+
+  import scala.concurrent.ExecutionContext.Implicits.global
 
   private implicit val encoding: String = BackupService.encoding
-  private implicit val timeout: FiniteDuration = Timeout(5.seconds).duration
 
   private implicit def listWrites[W](implicit w: Writes[W]): Writes[List[W]] = Writes.list[W](w)
 
   val usr = (employees ++ students).map(_.toUniqueEntity)
-  val aps = assignmentPlans.map(_.toUniqueEntity)
   val crs = courses.map(_.toUniqueEntity)
   val deg = degrees.map(_.toUniqueEntity)
   val lapps = labworkApplications.map(_.toUniqueEntity)
   val labs = labworks.map(_.toUniqueEntity)
-  val rls = roles.map(_.toUniqueEntity)
   val rms = rooms.map(_.toUniqueEntity)
   val sems = semesters.map(_.toUniqueEntity)
-  val tts = timetables.map(_.toUniqueEntity)
-  val bls = blacklists.map(_.toUniqueEntity)
   val cards = reportCardEntries.map(_.toUniqueEntity)
   val auths = authorities.map(_.toUniqueEntity)
   val schedules = scheduleEntries.map(_.toUniqueEntity)
   val grps = groups.map(_.toUniqueEntity)
-  val evals = reportCardEvaluations.map(_.toUniqueEntity)
 
-  val jsonSeq = toJson(usr, aps, crs, deg, lapps, labs, rls, rms, sems, tts, bls, cards, auths, schedules, grps, evals)
+  val jsonSeq = toJson(usr, crs, deg, lapps, labs, rms, sems, cards, auths, schedules, grps)
 
   private val userDao = mock[UserDao]
   private val assignmentPlanDao = mock[AssignmentPlanDao]
@@ -71,37 +66,6 @@ final class BackupServiceSpec extends WordSpec with TestBaseDefinition {
     Try(file).map(_.mkdirs).map(_ => file)
   }
 
-  "A BackupService" should {
-
-    "return json based backup items from data source" in {
-      val result = backupItems
-
-      result.size shouldBe jsonSeq.size
-      jsonSeq.zipWithIndex.forall {
-        case (json, index) => json == result(index).data
-      } shouldBe true
-    }
-
-    "write backup item to give location" in {
-      val items = backupItems
-      val result = backupService.persist(items, destFolder.get, shouldOverride = false)
-
-      result match {
-        case Success(files) =>
-          assertBackup(files)
-        case Failure(e) =>
-          fail(s"backup-service should write to ${destFolder.get.getPath}, but failed with exception ${e.getLocalizedMessage}")
-      }
-    }
-
-    "backup json based backup items from data source and write them to given location" in {
-      val result = Await.result(backupService.backup(destFolder.get, shouldOverride = true), timeout)
-      assertBackup(result)
-    }
-
-    "restore backup" in {} // TODO
-  }
-
   override protected def beforeAll(): Unit = {
     super.beforeAll()
 
@@ -114,31 +78,56 @@ final class BackupServiceSpec extends WordSpec with TestBaseDefinition {
     FileUtils.deleteDirectory(destFolder.get)
   }
 
-  private def assertBackup(result: Vector[File]): Unit = {
-    result.size shouldBe jsonSeq.size
-    result.map(f => FileUtils.readFileToString(f, encoding)).zipWithIndex.forall {
-      case (jsonString, index) => jsonSeq(index) == Json.parse(jsonString)
-    } shouldBe true
+  "A BackupService" should {
+
+    "return json based backup items from data source" in {
+      async(backupItems) { result =>
+        result.size shouldBe jsonSeq.size
+        result.map(_.data) should contain theSameElementsAs jsonSeq
+      }
+    }
+
+    "write backup items to given location" in {
+      async(backupItems) { items =>
+        val result = backupService.persist(items, destFolder.get, shouldOverride = false)
+        assertBackup(result.success.value)
+      }
+    }
+
+    "backup items from data source and write them to given location" in {
+      async(backupService.backup(destFolder.get, shouldOverride = true))(assertBackup)
+    }
+
+    "restore backup" in {} // TODO
   }
 
-  private def backupItems: Vector[BackupItem[JsValue]] = {
+  private def assertBackup(files: Vector[File]): Unit = {
+    files.size shouldBe jsonSeq.size
+
+    val jsonFiles = files.map(f => Json.parse(FileUtils.readFileToString(f, encoding)))
+    jsonFiles should contain theSameElementsAs jsonSeq
+  }
+
+  private def backupItems: Future[Vector[BackupItem]] = {
     when(userDao.get()).thenReturn(Future.successful(usr))
-    when(assignmentPlanDao.get()).thenReturn(Future.successful(aps))
+    when(assignmentPlanDao.get()).thenReturn(Future.successful(Seq.empty))
     when(courseDao.get()).thenReturn(Future.successful(crs))
     when(degreeDao.get()).thenReturn(Future.successful(deg))
     when(labworkApplicationDao.get()).thenReturn(Future.successful(lapps))
     when(labworkDao.get()).thenReturn(Future.successful(labs))
-    when(roleDao.get()).thenReturn(Future.successful(rls))
+    when(roleDao.get()).thenReturn(Future.successful(Seq.empty))
     when(roomDao.get()).thenReturn(Future.successful(rms))
     when(semesterDao.get()).thenReturn(Future.successful(sems))
-    when(timetableDao.get()).thenReturn(Future.successful(tts))
-    when(blacklistDao.get()).thenReturn(Future.successful(bls))
+    when(timetableDao.get()).thenReturn(Future.successful(Seq.empty))
+    when(blacklistDao.get()).thenReturn(Future.successful(Seq.empty))
     when(reportCardEntryDao.get()).thenReturn(Future.successful(cards))
     when(authorityDao.get()).thenReturn(Future.successful(auths))
     when(scheduleEntryDao.get()).thenReturn(Future.successful(schedules))
     when(groupDao.get()).thenReturn(Future.successful(grps))
-    when(reportCardEvaluationDao.get()).thenReturn(Future.successful(evals))
+    when(reportCardEvaluationDao.get()).thenReturn(Future.successful(Seq.empty))
 
-    Await.result(backupService.backupItems, timeout)
+    backupService.backupItems
   }
-}*/
+
+  override protected def bindings: Seq[GuiceableModule] = Seq.empty
+}
